5429d1206e0c54417d66015decc5cde61cd1febd
msesmart <msesmart@gmail.com>
2016-01-23 23:42:59 -0500

    realize the parsing function for 1K+ projects. add words statistics and stopwords update.

--- PreprocessForWord2Vec.java
+++ PreprocessForWord2Vec.java
@@ -1,131 +1,44 @@
 /**
- * 
+ *  Author: 	Chengjun Yuan  
+ *				<cy3yb@virginia.edu>
+ *	Time:		Jan. 2016 
+ *	Purpose:	extract words from readme files and code files of a number of java projects.
+ *	HowToRun:	compile:	javac -cp '.:libstemmer.jar' PreprocessForWord2Vec.java
+				run:		java -cp '.:libstemmer.jar' PreprocessForWord2Vec projectsFolder
  */
-package analyzer;
+ 
 import java.io.*;
 import java.util.*;
-import json.JSONArray;
-import json.JSONException;
-import json.JSONObject;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.tartarus.snowball.SnowballStemmer;
 import org.tartarus.snowball.ext.englishStemmer;
-//import org.tartarus.snowball.ext.porterStemmer;
 
-import structures.Post;
-
-/**
- * @author hongning
- * Sample codes for demonstrating OpenNLP package usage 
- * NOTE: the code here is only for demonstration purpose, 
- * please revise it accordingly to maximize your implementation's efficiency!
- */
 public class PreprocessForWord2Vec {
-	
-	//a list of stopwords
-	int loadControl=0;
-	int reviews_size=0;
-	String reviewsFile="reviewsFile.csv";
-	PrintWriter reviewsWriter;
-	//static HashSet<String> all_stopwords;
-	//static HashSet<String> original_stopwords;
-	//static HashSet<String> new_stopwords;
-	//you can store the loaded reviews in this arraylist for further processing
-	List<Post> m_reviews;
-	List<HashMap<String,Integer>> reviewsTokenTF;
-	List<double[]> reviewsVector;
-	List<Map<String,Double>> reviewsSimilarities;
-	
-	Tokenizer tokenizer; // global tokenizer
-	SnowballStemmer stemmer; // global stemmer
-	HashMap<String,Integer> token_TTF;
-	HashMap<String,Integer> token_DF;
-	HashMap<String,Integer> controlVocabulary;
-	HashMap<String,Double> controlVocabulary_IDF;
-	List<String> unigramList;
-	List<int[]> bigramList;
-	HashMap<String,Integer> unigramIndex;
-	HashMap<String,Integer> unigramCount;
-	int numUnigram;
-	int numBigram;
-	double[] unigramLM;
-	int[][] bigramCount;
-	double[][] bigramLM_LIS; // Linear Interpolation Smoothing
-	double[][] bigramLM_ADS; // Absolute Discount Smoothing
-	double lamda;	// for Linear Interpolation Smoothing
-	double cigma;	// for Absolute Discount Smoothing
-	
-	int numLoadedFiles = 0；
+	int numLoadedFiles = 0;
 	int maxNumLoadedFiles = 2000;
 	StringBuilder codeSb, readMeSb;
-	
+	SnowballStemmer stemmer;
+	HashMap<String, Integer> wordsMap;
+	HashSet<String> stopWords;
+ 
 	public PreprocessForWord2Vec(){
-		//all_stopwords=new HashSet<String>();
-		//original_stopwords=new HashSet<String>();
-		//new_stopwords=new HashSet<String>();
-		m_reviews = new ArrayList<Post>();
-		reviewsTokenTF=new ArrayList<HashMap<String,Integer>>();
 		
-		reviewsVector=new ArrayList<double[]>();
-		reviewsSimilarities=new ArrayList<Map<String,Double>>();
-		stemmer = new englishStemmer();
-		try{
-			tokenizer = new TokenizerME(new TokenizerModel(new FileInputStream("./data/Model/en-token.bin")));
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-		token_TTF=new HashMap<String,Integer>();
-		token_DF=new HashMap<String,Integer>();
-		controlVocabulary=new HashMap<String,Integer>();
-		controlVocabulary_IDF=new HashMap<String,Double>();
-		unigramList=new ArrayList<String>();
-		unigramIndex=new HashMap<String,Integer>();
-		unigramCount=new HashMap<String,Integer>();
-		bigramList=new ArrayList<int[]>();
-		numUnigram=0;
-		numBigram=0;
-		lamda=0.9; cigma=0.1;
-		
-		try{
+		/*try{
 			reviewsWriter = new PrintWriter(new BufferedWriter(new FileWriter(reviewsFile, true)));
 		}catch (IOException e) {
 			e.printStackTrace();
-		}
+		}*/
 		
 		codeSb = new StringBuilder();
 		readMeSb = new StringBuilder();
+		stemmer = new englishStemmer();
+		wordsMap = new HashMap<String, Integer>();
+		stopWords = new HashSet<String>();
+		 
 	}
-	
-	//sample code for loading a list of stopwords from file
-	//you can manually modify the stopword file to include your newly selected words
-	
-	
-	public void analyzeDocumentDemo(JSONObject json){
-		try {
-			PrintWriter writer = new PrintWriter("temp.txt", "UTF-8");
-			JSONArray jarray = json.getJSONArray("Reviews"); int j=0;
-			for(int i=0; i<jarray.length(); i++) {
-				Post review = new Post(jarray.getJSONObject(i));
-				//System.out.println(review.getID()+"  "+review.getAuthor());
-				//List<String> tokens=TokenizerNormalizationStemming(review.getContent()); 
-				j=TokenizerNormalizationStemming(review.getContent()); 
-				// HINT: perform necessary text processing here, e.g., tokenization, stemming and normalization
-				if(j>0){
-					//m_reviews.add(review);
-					reviews_size++;
-					reviewsWriter.println(review.getAuthor()+","+review.getDate()+","+review.getContent());
-				}
-			}
-			writer.close();
-        }catch(FileNotFoundException ex){
-            ex.printStackTrace();
-        }catch(IOException e){
-        	e.printStackTrace();
-        }catch(JSONException e){
-			e.printStackTrace();
-		}
-	}
-	
 	
 	public String LoadReadMeFile(String fileName) {
 		try {
@@ -140,22 +53,29 @@ public class PreprocessForWord2Vec {
 			return sb.toString();
 		}catch(FileNotFoundException ex) {
             ex.printStackTrace();
+			return null;
         }catch(IOException e) {
         	e.printStackTrace();
+			return null;
         }
 	}
 	
 	public String parseConnectedWords(String text) {
 		StringBuilder sb = new StringBuilder();
-		int start = 0;
-		for(int i = 0; i < text.length(); i++) {
+		int start = 0; int i = 0;
+		for(i = 0; i < text.length(); i++) {
 			if(Character.isUpperCase(text.charAt(i))) {
-				sb.append(text.substring(start, i).toLowerCase());
-				sb.append(" ");
+				String token = text.substring(start, i).toLowerCase();
+				stemmer.setCurrent(token);
+				if(stemmer.stem()) token = stemmer.getCurrent();
+				sb.append(token); sb.append(" ");
 				start = i;
 			}
 		}
-		sb.append(text.substring(start, i).toLowerCase() + " ");
+		String token = text.substring(start, i).toLowerCase();
+		stemmer.setCurrent(token);
+		if(stemmer.stem()) token = stemmer.getCurrent();
+		sb.append(token); sb.append(" ");
 		return sb.toString();
 	}
 	
@@ -164,7 +84,9 @@ public class PreprocessForWord2Vec {
 		StringBuilder sb = new StringBuilder();
 		for(String token : tokens) {
 			if(token.equals(token.toLowerCase()) || token.equals(token.toUpperCase())) {
-				sb.append(token.toLowerCase());
+				stemmer.setCurrent(token.toLowerCase());
+				if(stemmer.stem()) token = stemmer.getCurrent();
+				sb.append(token);
 				sb.append(" ");
 			} else {
 				sb.append(parseConnectedWords(token)); 
@@ -178,7 +100,9 @@ public class PreprocessForWord2Vec {
 		StringBuilder sb = new StringBuilder();
 		for(String token : tokens) {
 			if(token.equals(token.toLowerCase()) || token.equals(token.toUpperCase())) {
-				sb.append(token.toLowerCase());
+				stemmer.setCurrent(token.toLowerCase());
+				if(stemmer.stem()) token = stemmer.getCurrent();
+				sb.append(token);
 				sb.append(" ");
 			} else {
 				sb.append(parseConnectedWords(token)); 
@@ -192,22 +116,24 @@ public class PreprocessForWord2Vec {
 			StringBuilder sb = new StringBuilder();
 			BufferedReader in = new BufferedReader(new FileReader(fileName));
 			String str = in.readLine();
-			str = str.trim();  // remove the leading and trailing spaces.
+			
 			while(str != null) {
+				str = str.trim();  // remove the leading and trailing spaces.
 				if(str.startsWith("import")) {
 					sb.append(parseImportedPackages(str));
 				}else if(str.startsWith("public") || str.startsWith("private") || str.startsWith("class") || str.startsWith("void")) {
 					sb.append(parseClassMethods(str));
 				} else ;				
 				str = in.readLine();
-				str = str.trim();
 			}
 			in.close();
 			return sb.toString();
 		}catch(FileNotFoundException ex) {
             ex.printStackTrace();
+			return null;
         }catch(IOException e) {
         	e.printStackTrace();
+			return null;
         }
 	}
 	
@@ -217,24 +143,23 @@ public class PreprocessForWord2Vec {
 		for(File f:dir.listFiles()){
 			if(f.isFile()) {
 				if(f.getName().toLowerCase().startsWith(prefix)) {
-					System.out.println(numLoadedFiles + " load README file"+" : " + f.getName());
-					readMeSb.append(LoadReadMeFile(f.getName()));
+					//System.out.println(numLoadedFiles + " load README file"+" : " + f.getName());
+					readMeSb.append(LoadReadMeFile(f.getAbsolutePath()));
 					numLoadedFiles ++;
 				}
 				else if(f.getName().endsWith(suffix)) {
-					System.out.println(numLoadedFiles + " load code file"+" : "+f.getName());
-					codeSb.append(loadCodeFile(f.getName()));
+					//System.out.println(numLoadedFiles + " load code file"+" : "+f.getName());
+					codeSb.append(loadCodeFile(f.getAbsolutePath()));
 					numLoadedFiles ++;
 				}
 			}
 			else if(f.isDirectory())
 				LoadFilesFromFolder(f.getAbsolutePath(), prefix, suffix);
 		}
-		reviewsWriter.close();
+		//reviewsWriter.close();
 	}
 	
 	// save String data into file under folder 
-	
 	public void saveStringIntoFileUnderFolder(String folder, String fileName, String str) {
 		try {
             File dir = new File(folder); 
@@ -252,26 +177,31 @@ public class PreprocessForWord2Vec {
 	// parse each project folder and save the preprocessed data in their own folder.
 	public void parseProjects(String folder) {
 		File dir = new File(folder); 
+		int i = 1;
 		for(File f:dir.listFiles()){ 
 			/* detect project folder */
 			if(f.isDirectory()) {
+				System.out.println("project #"+ String.valueOf(i) + f.getAbsolutePath());
 				readMeSb.setLength(0);
 				codeSb.setLength(0);
-				LoadFilesFromFolder(f, "readme", "java");
-				saveStringIntoFileUnderFolder(f, "re.prepro", readMeSb.toString());
-				saveStringIntoFileUnderFolder(f, "code.prepro", codeSb.toString());
+				LoadFilesFromFolder(f.getAbsolutePath(), "readme", "java");
+				saveStringIntoFileUnderFolder(f.getAbsolutePath(), "re.prepro", readMeSb.toString());
+				saveStringIntoFileUnderFolder(f.getAbsolutePath(), "code.prepro", codeSb.toString());
 			}
+			i ++;
+			//if(i >= 2) break;
 		}
 	}
 	
 	public String TokenizerNormalizationStemming(String text){
 		if(text == null || text.length() == 0)return "";
 		StringBuilder sb = new StringBuilder();
-		for(String token : tokenizer.tokenize(text)) {
+		String[] tokens = text.split("[^a-zA-Z']+");
+		for(String token : tokens) {
 			//token=token.replaceAll("\\W+", "");
-			token = token.replaceAll("\\p{Punct}+","");
+			//token = token.replaceAll("\\p{Punct}+","");
 			token = token.toLowerCase();
-			if(token.matches(".*\\d+.*")) token = "";
+			//if(token.matches(".*\\d+.*")) token = "";
 			if(!token.isEmpty()){
 				stemmer.setCurrent(token);
 				if(stemmer.stem()) token = stemmer.getCurrent();
@@ -282,8 +212,97 @@ public class PreprocessForWord2Vec {
 		return sb.toString();
 	}
 
+	public void WordsStatics(String folder) {
+		try {
+			System.out.println("start words statics...");
+			File dir = new File(folder); 
+			for(File f:dir.listFiles()){ 
+				/* detect project folder */
+				if(f.isDirectory()) {
+					String line = new String(Files.readAllBytes(new File(f, "re.prepro").getAbsolutePath()));
+					String[] tokens = line.split(" ");
+					for(String token : tokens) {
+						if(token != null && token.length() > 0) {
+							if(wordsMap.containsKey(token)) wordsMap.put(token, wordsMap.get(token) + 1);
+							else wordsMap.put(token, 1);
+						}
+					}
+					line = new String(Files.readAllBytes(new File(f, "code.prepro").getAbsolutePath()));
+					tokens = line.split(" ");
+					for(String token : tokens) {
+						if(token != null && token.length() > 0) {
+							if(wordsMap.containsKey(token)) wordsMap.put(token, wordsMap.get(token) + 1);
+							else wordsMap.put(token, 1);
+						}
+					}
+				}
+			}
+			System.out.println("Finish words statics !! ");
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public void generateStopWords(String stopWordsFileName, int offset) {
+		wordsMap = sortByComparator(wordsMap); // sort the wordsMap based on value
+		try {
+			/* write wordsStatics into file */
+			PrintWriter writer = new PrintWriter("wordsStatics.txt", "UTF-8");
+			for(Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
+				writer.println(entry.key() + "," + String.valueOf(entry.value()));
+				if(entry.value() > offset) stopWords.add(entry.key());
+			}
+			writer.close();
+			/* load old stopWords file */
+			String[] words = Files.readAllLines(stopWordsFileName, StandardCharsets.UTF_8);
+			for(String word : words) {
+				if(!word.isEmpty()) {
+					stemmer.setCurrent(word.toLowerCase());
+					if(stemmer.stem()) word = stemmer.getCurrent();
+					if(!stopWords.contains(word)) stopWords.add(word);
+				}
+			}
+        }catch(FileNotFoundException ex){
+            ex.printStackTrace();
+        }catch(IOException e){
+        	e.printStackTrace();
+        }
+	}
+	
+	public void removeStopWords(String folder) {
+		try {
+			System.out.println("start remove stopWords...");
+			File dir = new File(folder); 
+			StringBuilder sb = new StringBuilder();
+			for(File f:dir.listFiles()){ 
+				/* detect project folder */
+				if(f.isDirectory()) {
+					String line = new String(Files.readAllBytes(new File(f, "re.prepro").getAbsolutePath()));
+					String[] tokens = line.split(" ");
+					for(String token : tokens) {
+						if(token != null && token.length() > 0) {
+							if(wordsMap.containsKey(token)) wordsMap.put(token, wordsMap.get(token) + 1);
+							else wordsMap.put(token, 1);
+						}
+					}
+					line = new String(Files.readAllBytes(new File(f, "code.prepro").getAbsolutePath()));
+					tokens = line.split(" ");
+					for(String token : tokens) {
+						if(token != null && token.length() > 0) {
+							if(wordsMap.containsKey(token)) wordsMap.put(token, wordsMap.get(token) + 1);
+							else wordsMap.put(token, 1);
+						}
+					}
+				}
+			}
+			System.out.println("Finish remove stopWords !! ");
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
 	// sort HashMap by value
-	private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
+	private Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
 		// Convert Map to List
 		List<Map.Entry<String, Integer>> list =new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
 		// Sort list with comparator, to compare the Map values
@@ -336,9 +355,12 @@ public class PreprocessForWord2Vec {
 	}
 	
 	public static void main(String[] args)throws IOException{		
-		
+		if(args.length != 1) {
+			System.out.println("input command : java PreprocessForWord2Vec folder");
+			System.exit(0);
+		}
 		PreprocessForWord2Vec preprocess = new PreprocessForWord2Vec();
-		preprocess.parseProjects();
+		preprocess.parseProjects(args[0]);
 		System.out.println("Done ");
 	}
 }
