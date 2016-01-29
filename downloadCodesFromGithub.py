1b8aa85bb088852731c87fdd4e8bc7fa323dafa4
msesmart <msesmart@gmail.com>
2016-01-29 11:08:54 -0500

    add main funtion, sys.argv, change directory.

--- downloadCodesFromGithub.py
+++ downloadCodesFromGithub.py
@@ -3,7 +3,13 @@ import shlex
 import csv
 import timeit
 import time
-import os
+import os, sys
+
+'''
+	Author: Chengjun Yuan <cy3yb@virginia.edu>
+	Time:	Jan.29.2016
+	how to run: python downloadCodesFromGithub.py projectsList.csv downloadProjectsPath
+'''
 
 # read URLs from javaProjectsURLs.csv 
 def readUrls(fileName):
@@ -12,7 +18,7 @@ def readUrls(fileName):
 	URLs = []
 	i = 0
 	for row in csvRows:
-		if i == 0:		# The first row is column name, not project information.
+		if i == 0:		# The first row is column 
 			i += 1
 			continue
 		index = row[1].find('repos') + 6
@@ -25,28 +31,34 @@ def readUrls(fileName):
 	csvFile.close
 	return URLs
 
-URLs = readUrls('javaProjectsURLs.csv')
-print 'URL',len(URLs)
-
-i=0
-for oneURL in URLs:
-	i = i + 1
-	if i > 100:  # the number of projects to download
-		break
-	index = oneURL.find('/')
-	folder = oneURL[index + 1 :]
-	if os.path.isdir(folder) == True : # if the project has been downloaded, then pass to the next project
-		continue
-	command = 'git clone git@github.com:'+oneURL+'.git'
-	print command
-	args = shlex.split(command)
-	startTime = timeit.default_timer()
-	p = subprocess.Popen(args)
-	p.wait()
-	usedTime = timeit.default_timer() - startTime
-	if usedTime < 3 :
-		time.sleep(3 - usedTime)
-	
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "Usage: python downloadCodesFromGithub.py projectsList.csv downloadProjectsPath"
+		sys.exit()
+	URLs = readUrls(sys.argv[1])
+	print 'URL',len(URLs)
+	os.chdir(sys.argv[2])
+	retval = os.getcwd()
+	print "Directory changed to %s" % retval
+	i=0
+	for oneURL in URLs:
+		i = i + 1
+		if i > 1000:  # the number of projects to download
+			break
+		index = oneURL.find('/')
+		folder = oneURL[index + 1 :]
+		if os.path.isdir(folder) == True : # if the project has been downloaded, then pass to the next project
+			continue
+		command = 'git clone git@github.com:'+oneURL+'.git'
+		print i, command
+		args = shlex.split(command)
+		startTime = timeit.default_timer()
+		p = subprocess.Popen(args)
+		p.wait()
+		usedTime = timeit.default_timer() - startTime
+		if usedTime < 3 :
+			time.sleep(3 - usedTime)
+		
 	
 
 	
